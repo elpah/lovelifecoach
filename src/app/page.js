0de5d87961de78e559795e47e72da9ffb0ef467c
Elpah <obengelpachris@gmail.com>
2024-03-29 03:36:23 +0100

    saving progress

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,28 @@
 import Image from "next/image";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className="min-h-screen ">
-      <div className="flex justify-center items-center md:flex-row h-screen bg-cover-photo bg-no-repeat bg-cover  w-full">
-        <h1>LOREM IPSUM</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim
+    <main className={styles.main}>
+      <div className={styles.header_div}>
+        <p className={styles.intro_paragraph}>FREE INTRODUCTORY MINI SECTION</p>
+        <h1 className={styles.header}>
+          Creating Calm Lorem Ipsum
+          <br /> Amidst Life'storm
+        </h1>
+        <p className={styles.sub}>
+          Discover how our therapeutic approach can help you find inner peace
+          and <br />
+          resilience, even in the midst of life's challenges.
         </p>
+        <div className={styles.button_container}>
+          <button className={`${styles.button} ${styles.button_lorem}`}>
+            Lorem
+          </button>
+          <button className={`${styles.button} ${styles.button_ipsum}`}>
+            Ipsum
+          </button>
+        </div>
       </div>
     </main>
   );
