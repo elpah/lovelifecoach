58d1a63b30099402492ff5d4a527cf95247ab6c5
Elpah <obengelpachris@gmail.com>
2024-03-31 01:45:15 +0100

    improved homepage, added cover image

--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,33 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.header_div}>
-        <p className={styles.intro_paragraph}>FREE INTRODUCTORY MINI SECTION</p>
-        <h1 className={styles.header}>
-          Creating Calm Lorem Ipsum
-          <br /> Amidst Life'storm
-        </h1>
-        <p className={styles.sub}>
-          Discover how our therapeutic approach can help you find inner peace
-          and <br />
-          resilience, even in the midst of life's challenges.
-        </p>
-        <div className={styles.button_container}>
-          <button className={`${styles.button} ${styles.button_lorem}`}>
-            Lorem
-          </button>
-          <button className={`${styles.button} ${styles.button_ipsum}`}>
-            Ipsum
-          </button>
+      <div className={styles.cover_container}>
+        <div className={styles.header_container}>
+          <div className={styles.header_div}>
+            <p className={styles.intro_paragraph}>
+              Talk to a professional Therapist...
+            </p>
+            <h1 className={styles.header}>
+              BLESS KOFI ASIEDU
+              <br />
+              The <span className={styles.header_span}>LoveLife Coach</span>
+            </h1>
+            <p className={styles.sub}>
+              Direction is what you need when you are lost <br />
+              Not SPEED! The Cost involved in dealing with the...
+            </p>
+            <div className={styles.button_container}>
+              <button className={`${styles.button} ${styles.button_lorem}`}>
+                Email
+              </button>
+              <button className={`${styles.button} ${styles.button_ipsum}`}>
+                Book Appointment
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.image_container}>
+          <img src="/images/bka.jpg" />
         </div>
       </div>
     </main>
