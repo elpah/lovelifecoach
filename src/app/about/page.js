6b97de5a448b4feb0b5f1745d7fc37d9e4898aa3
Elpah <obengelpachris@gmail.com>
2024-04-07 03:07:06 +0200

    contact page progress

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -71,9 +71,33 @@ export default function page() {
             </div>
             <div className={styles.why_badge}>
               <FaCheck className={styles.badge_check} />
-              <p className={styles.badge_text}>Counseling since 2005</p>
+              <p className={styles.badge_text}>Expertise in Relationships</p>
             </div>
           </div>
+        </div>
+      </section>
+      <section className={styles.about_section_container}>
+        <div className={styles.about_text_container}>
+          <h2 className={styles.about_header}>
+            Building Trust, Nurturing Hope.
+          </h2>
+          <p className={styles.about_text}>
+            Bless Kofi Asiedu LOREM IPSUM Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt molli
+          </p>
+          <p className={styles.signature}>Bless Kofi Asiedu</p>
+        </div>
+        <div className={styles.about_image_container}>
+          <img
+            className={styles.about_image}
+            src="/images/bka.jpg"
+            alt="counsellor's Photo"
+          />
         </div>
       </section>
     </div>
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,49 @@
+"use client";
+import { FaPhone, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import styles from "./contact.module.scss";
+
 export default function page() {
-  return <div>contact page</div>;
+  return (
+    <div className={styles.contact_container}>
+      <div className={styles.contact_info}>
+        <h2 className={styles.header}>Contact</h2>
+        <p className={styles.name}>BLESS KOFI ASIEDU</p>
+        <p className={styles.credentials}>
+          Health Psychologist | Counsellor | Lovelife Coach | Therapist
+        </p>
+        <div className={styles.card_container}>
+          <div className={styles.contact_card}>
+            <div className={styles.fa_container}>
+              {" "}
+              <FaPhone className={styles.fa_icon} />
+            </div>
+            <p className={styles.card_text}>+233 23 2930293</p>
+          </div>
+          <div className={styles.contact_card}>
+            <div className={styles.fa_container}>
+              <FaLinkedin className={styles.fa_icon} />
+            </div>
+
+            <p className={styles.card_text}>BKA</p>
+          </div>
+          <div className={styles.contact_card}>
+            <div className={styles.fa_container}>
+              <FaEnvelope className={styles.fa_icon} />
+            </div>
+
+            <p className={styles.card_text}>BKA@fiQEAFD.COM</p>
+          </div>
+        </div>
+        <p className={styles.sub_text}>
+          If you would like some lorem ipsum test or more lorem ipsim dont
+          hesitate to contact me
+        </p>
+      </div>
+      <div className={styles.contact_form}>
+        <form>
+          <label>Name</label>
+        </form>
+      </div>
+    </div>
+  );
 }
