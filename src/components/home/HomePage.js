413829fc9f802970c5b485d0e812e718b53ad4fa
Elpah <obengelpachris@gmail.com>
2024-04-14 15:18:57 +0200

    saving progress

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -7,17 +7,19 @@ export default function HomePage() {
       <div className={styles.details_container}>
         <p className={styles.i_am}>I AM</p>
         <h2 className={styles.name}>BLESS KOFI ASIEDU</h2>
-        <h3>
-          A <span>PROFESSIONAL THERAPIST</span>
+        <h3 className={styles.title}>
+          A <span className={styles.title_tag}>PROFESSIONAL THERAPIST</span>
         </h3>
-        <p>
+        <p className={styles.paragraph}>
           Direction is what you need when you are lost and NOT SPEED! The cost
           involved in dealing with the issues of life have to be prioritized and
           confronted.
         </p>
-        <button>Book Appointment</button>
+        <button className={styles.button}>Book Appointment</button>
       </div>
-      <div></div>
+      <div className={styles.image_container_bg}>
+        <img className={styles.ceo_img} src="/images/ceo/bka.png" />
+      </div>
     </main>
   );
 }
