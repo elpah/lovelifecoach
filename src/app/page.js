63e404d82950f84791f23ee8aaeb8980432fd3fd
Elpah <obengelpachris@gmail.com>
2024-04-17 02:41:48 +0200

    made some changes to services section

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,9 @@ export default function page() {
       <Navbar />
       <HomePage />
       <AboutMe />
-      {/* 
       <Services />
+      {/* 
+      
       <AboutMe />
       <ContactSection /> */}
     </>
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -27,55 +27,23 @@ export default function Services() {
   ];
   return (
     <div className={styles.main_container}>
-      <h1 className={styles.header}>
-        What do you do when there seem to be nothing to do?
-      </h1>
-      <p className={styles.paragraph}>
-        Explore the significance of counseling in promoting mental health,
-        emotional well-being, and personal growth.
-      </p>
-
-      <section className={styles.why_section}>
-        <div className={styles.why_image_container}>
-          <img
-            className={styles.why_image}
-            src="https://media.istockphoto.com/id/961556200/photo/smiling-african-american-psychiatrist-talking-to-young-couple.jpg?s=612x612&w=0&k=20&c=-rdM2vWMyJzOz_iG88iu1lpBkmobihfr8iRZXc3iXB4="
-          />
+      <div className={styles.container}>
+        <div className={styles.header_container}>
+          <h2 className={styles.header_shadow}>MY SERVICES</h2>
+          <h3 className={styles.name}>WHAT I DO</h3>
         </div>
-        <div className={styles.why_text_div}>
-          <div className={styles.small_header_container}>
-            <FaQuestion className={styles.icon} />
-            <p className={styles.small_header}>Why you need Counseling</p>
-          </div>
-          <h2 className={styles.why_header}>
-            Your Journey to Wellness Begins Here
-          </h2>
-          <p className={styles.why_text}>
-            We're here to support your personal growth and empower you to find
-            lasting peace in a welcoming, friendly atmosphere.
-          </p>
-          <div className={styles.why_badge_container}>
-            <div className={styles.why_badge}>
-              <FaCheck className={styles.badge_check} />
-              <p className={styles.badge_text}>Counseling since 2005</p>
-            </div>
-            <div className={styles.why_badge}>
-              <FaCheck className={styles.badge_check} />
-              <p className={styles.badge_text}>Expertise in Relationships</p>
-            </div>
-          </div>
+        <p className={styles.services_paragraph}>
+          Explore the significance of counselling in promoting mental health,
+          emotional well-being, and personal growth. Bless Kofi Asiedu helps you
+          heal past hurts and equips you with the tools to build fulfilling,
+          lasting relationships
+        </p>
+        <div className={styles.services_container}>
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
+          <ServiceCard />
         </div>
-      </section>
-      <h2 className={styles.service_header}>What I do.</h2>
-
-      <div className={styles.service_container}>
-        {services.map((service, index) => (
-          <ServiceCard
-            key={index}
-            cardName={service.serviceName}
-            cardImage={service.serviceImage}
-          />
-        ))}
       </div>
     </div>
   );
